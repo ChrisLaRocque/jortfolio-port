64d4fda4dda4f6d7534db95b555c13631f5cc0aa
Chris LaRocque <larocque.christopher@gmail.com>
2023-02-28 20:03:25 -0500

    Update SEO.jsx

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -19,7 +19,7 @@ function SEO({
       }}
       title={title}
       meta={[
-      {name: 'robots', content: 'noindex, nofollow'}
+      {name: 'robots', content: 'noindex, nofollow'},
         {
           name: 'description',
           content: description,
